1ff6a444bf37d1c5947531801e7dc24c749ed81b
Jaime Torres <jatber.mrowr@gmail.com>
2024-10-08 08:48:59 -0500

    add example tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import { createRoot } from 'react-dom/client';
+import Checkbox from './components/checkbox';
+
+const container = document.getElementById("root");
+const root = createRoot(container);
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -15,3 +20,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+root.render(<Checkbox labelActive="Active" labelInactive="Inactive" />)
